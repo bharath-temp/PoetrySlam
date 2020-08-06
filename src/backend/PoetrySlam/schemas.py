e02407561ee0685e0ae17b32a0c38009c58a2196
Rushil Vora <rushilvora@hotmail.com>
2020-08-06 01:38:40 -0700

    feat(frontend) switch to MaterialUI and add pages

--- src/backend/PoetrySlam/schemas.py
+++ src/backend/PoetrySlam/schemas.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from uuid import UUID
+from enum import IntEnum
 
 from pydantic import BaseModel
 
@@ -14,3 +15,23 @@ class User(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class UserProfile(BaseModel):
+	id: UUID
+	user_name: str
+	full_name: str
+	location: str
+	years_experience: str
+	
+
+
+class PoemEntries(BaseModel):
+	id: UUID
+	user_name: str
+	contentBody: str
+	tags: Set[str] = set()
+	timePublished: datetime = None
+
+	class Config:
+		orm_mode = True
